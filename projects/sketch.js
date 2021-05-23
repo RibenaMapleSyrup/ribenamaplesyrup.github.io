ba92d2e93bfac2c1716204ce6f7c276c5b46d4c2
Sean Greaves <seangreaves@Seans-MBP.fritz.box>
2021-05-23 23:33:06 +0100

    from tiles to sprinkles

--- projects/sketch.js
+++ projects/sketch.js
@@ -1,28 +1,27 @@
-let dancer;
-const waves = [];
-const ocean = 30
+let sprinkle;
+const cake = [];
+const sprinkles = 30
 
 function setup() {
   createCanvas(800, 800);
-  background(0);
+  // background(0);
 
   w = 3/4*width
   h = 3/4*height
 
-  for (let i=0; i<ocean; i++) {
-    for (let j=0; j<ocean; j++) {
-      waves.push(new Dancer(i*w/ocean+100, j*h/ocean+100, 600/(2*ocean),0.06,9*(j*j+i*i)))
+  for (let i=0; i<sprinkles; i++) {
+    for (let j=0; j<sprinkles; j++) {
+      cake.push(new Sprinkle(i*w/sprinkles+100,j*h/sprinkles+40,0.0005+j*0.0001,200,200,0.00051+i*0.0001,0,0))
     }
   }
+
 }
 
 function draw() {
   background('#171414');
-  stroke(255);
-  strokeWeight(1);
-
-  for (let dancer of waves) {
-    dancer.transform()
-    dancer.illuminate()
+  for (let sprinkle of cake) {
+    sprinkle.update();
+    sprinkle.translate();
+    sprinkle.show();
   }
 }
